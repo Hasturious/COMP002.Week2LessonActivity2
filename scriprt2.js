cdfe0e3a2e2ce05ce07989b105aed0b3916e5c58
Hasturious <jbster559@gmail.com>
2024-10-23 20:13:44 -0700

    Added 2nd script

---
+++ scriprt2.js
@@ -0,0 +1,14 @@
+let myGrade = 100
+
+if (myGrade >= 90) {
+    console.log("A");
+}
+else if (myGrade =< 89 && > 80) {
+    console.log("B");
+}
+else if (myGrade =< 79 && > 70) {
+    console.log("C");
+}
+else if (myGrade =< 69 && > 60) {
+    console.log("D");
+}
