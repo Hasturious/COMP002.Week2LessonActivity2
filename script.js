b260fa5964bc19ba397db7e8ae2f924b9f48c572
Hasturious <jbster559@gmail.com>
2024-10-23 19:42:15 -0700

    Added console logs and vars

---
+++ script.js
@@ -0,0 +1,11 @@
+let myGreeting = "Hello World!";
+
+console.log(myGreeting);
+
+const pi = 3.14;
+
+// pi = 3 THIS // RESULTS IN AN ERROR (Assignment to constant variable)
+
+let myNumber = 6
+
+console.log(Math.min(pi, myNumber));
