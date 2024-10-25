ca07585f4dc462574f229421440990285378d029
Jean Bryant Figueroa <jbster559@gmail.com>
2024-10-24 21:38:36 -0700

    Added switchcase

--- scriprt2.js
+++ scriprt2.js
@@ -14,10 +14,25 @@ else if (myGrade <= 69 && myGrade >= 60) {
 }
 else console.log("F")
 
-switch (prompt(50)){
-    case 
-    break;
+switch (myGrade) {
+    case (myGrade >= 90 && myGrade <= 100):
+        console.log("A");
+        break;
+    case (myGrade >= 80 && myGrade < 90):
+        console.log("B");
+        break;
+    case (myGrade >= 70 && myGrade < 80):
+        console.log("C");
+        break;
+    case (myGrade >= 60 && myGrade < 70):
+        console.log("D");
+        break;
+    case (myGrade >= 0 && myGrade < 60):
+        console.log("F");
+        break;
 }
+
+// Im a bit more bias to if/else statements as they are easier to read but I understand the utility and clean looking switch case
 
 for(let i = 0;i <=100; i ++) {
     if (i%2 == 1){
